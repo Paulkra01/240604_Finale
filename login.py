ac1d9b1ed69764055686aaf8e9459db131fd56bd
Paulkra01 <krakis007@gmail.com>
2024-06-12 12:17:06 +0200

    update login file

--- login.py
+++ login.py
@@ -9,34 +9,40 @@ import read_person_data as rpd
 import ekgdata as ekg
 import person
 
-#Login funktion som kollar om användaren finns i databasen och om lösenordet stämmer
-def login():st.set_page_config(page_title="Welcome", page_icon=":open_hands:")
-
 # Lade alle Personen
 person_names = rpd.get_person_list(rpd.load_person_data())
 
+#Login funktion som kollar om användaren finns i databasen och om lösenordet stämmer
+st.set_page_config(layout="centered", page_title="Welcome", page_icon=":open_hands:")
 
-# Anlegen diverser Session States
-## Gewählte Versuchsperson
-if 'aktuelle_versuchsperson' not in st.session_state:
-    st.session_state.aktuelle_versuchsperson = 'None'
+st.title("Login")
 
-## Anlegen des Session State. Bild, wenn es kein Bild gibt
-if 'picture_path' not in st.session_state:
-    st.session_state.picture_path = 'data/pictures/none.jpg'
 
-col1, col2, col3 = st.columns(3) 
-    
-with col1:  
-        st.session_state.aktuelle_versuchsperson = st.selectbox(
-            'Versuchsperson',
-            options = person_names, key="sbVersuchsperson")
-        # Name der Versuchsperson
-        selected_person = st.session_state.aktuelle_versuchsperson
+# Dropdown-Menü für den Benutzernamen mit Vorschlägen
+username = st.selectbox("Benutzername", person_names)
 
-with col2:
-    person_birthyear = rpd.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)['date_of_birth']
-    st.write(f"Geburtsjahr: {person_birthyear} ")
+# Texteingabefeld für neuen Benutzernamen
+new_username = st.text_input("Neuer Benutzername")
 
-with col3:
-    person_id = rpd.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)["id"]
+# Passwort-Eingabefeld
+password = st.text_input("Passwort", type="password")
+
+
+
+# Button zum Einloggen
+login_button = st.button("Login")
+# Button zum Speichern eines neuen Benutzernamens
+save_button = st.button("Als neues Profil speichern")
+
+# Verarbeiten der Anmeldeinformationen, wenn der Button gedrückt wird
+if login_button:
+    if username == "user1" and password == "password":
+        st.success("Erfolgreich eingeloggt")
+        # Weitere Aktionen nach erfolgreichem Login
+    else:
+        st.error("Falscher Benutzername oder falsches Passwort")
+
+# Verarbeiten des neuen Benutzernamens, wenn der Button gedrückt wird
+if save_button and new_username:
+    usernames.append(new_username)
+    st.success(f"Neuer Benutzer '{new_username}' erfolgreich erstellt")
